2969e2e7294e3ff51e670701cc71a420319ef250
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-04-27 22:47:20 +0300

    add custom app bar to home

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:storeapp/core/utils/assets.dart';
+
+class CustomAppBr extends StatelessWidget {
+  const CustomAppBr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 24,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
