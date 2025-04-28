139f5a3c53bc161eeaa6fc8e8ad067a30cfcdd3f
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-04-28 23:09:57 +0300

    add books featured list view to home body replacing the item

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,17 +6,14 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
-      child: AspectRatio(
-        aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              image: AssetImage(
-                AssetsData.testImage,
-              ),
+    return AspectRatio(
+      aspectRatio: 2.6 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            image: AssetImage(
+              AssetsData.testImage,
             ),
           ),
         ),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:storeapp/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:storeapp/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:storeapp/features/home/presentation/views/widgets/featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBr(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
