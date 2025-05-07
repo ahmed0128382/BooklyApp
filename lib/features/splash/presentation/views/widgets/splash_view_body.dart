2ad48559d08ff301a7ba33e980b0f6ab5430275c
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-05-07 13:10:34 +0300

    usage of JSON to Dart extension with equatable package for book model

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -54,6 +54,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
+      // ignore: use_build_context_synchronously
       GoRouter.of(context).push(AppRouter.kHomeView);
       // Get.to(() => const HomeView(),
       //     transition: Transition.fade, duration: kTransitionDuration);
