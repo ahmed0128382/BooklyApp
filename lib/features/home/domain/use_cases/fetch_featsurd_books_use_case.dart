e4aa61d099ddab74d9068e9d6136b8a7f523b597
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-05-14 15:06:52 +0300

    implement fetch Newest and relevance books use cases

--- lib/features/home/domain/use_cases/fetch_featsurd_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featsurd_books_use_case.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:storeapp/core/errors/failure.dart';
 import 'package:storeapp/features/home/domain/entities/book_entity.dart';
 import 'package:storeapp/features/home/domain/repos/home_repo.dart';
-import 'package:storeapp/features/home/domain/use_cases/use_case.dart';
+import 'package:storeapp/core/use_cases/use_case.dart';
 
 class FetchFeatsurdBooksUseCase extends UseCase<List<BookEntity>, NoParameter> {
   final HomeRepo homeRepo;
---
+++ lib/features/home/domain/use_cases/fetch_relevance_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:storeapp/core/errors/failure.dart';
+import 'package:storeapp/core/use_cases/no_param_use_case.dart';
+import 'package:storeapp/features/home/domain/entities/book_entity.dart';
+import 'package:storeapp/features/home/domain/repos/home_repo.dart';
+
+class FetchRelevanceBooksUseCase extends UseCase<List<BookEntity>> {
+  final HomeRepo homeRepo;
+  final String category;
+  FetchRelevanceBooksUseCase(this.category, {required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchRelevanceBooks(category: category);
+  }
+}
